3b83dbddc0f565611915606d4a7c1b2350408f0d
Ducphat12345 <phatngduc@gmail.com>
2023-12-01 14:30:40 +0700

    update-response

--- src/main/java/com/poly/controller/CategoryController.java
+++ src/main/java/com/poly/controller/CategoryController.java
@@ -1,13 +1,12 @@
 package com.poly.controller;
 
+import com.poly.dto.ResponseObject;
 import com.poly.entity.Category;
 import com.poly.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -17,8 +16,11 @@ public class CategoryController {
     CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<Category>> getAllCategory() {
-        return new ResponseEntity<>(categoryService.getAllCategory(), HttpStatus.OK);
+    public ResponseEntity<?> getAllCategory() {
+        if (!categoryService.getAllCategory().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject(HttpStatus.OK,"Success!", categoryService.getAllCategory()));
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseObject(HttpStatus.NO_CONTENT, "Fail!", null));
     }
 
     @GetMapping("/search-by-id/{id}")
